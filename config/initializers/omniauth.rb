de3c186c201c969263d48daff1e55d7c4c3e64fe
justin <justin.ryan.holmes@icloud.com>
2015-08-08 21:09:59 -0600

    Add require rspotify in omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+require 'rspotify/oauth'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], scope: 'playlist-modify-public playlist-modify-private playlist-read-private user-read-private user-read-email'
 end
