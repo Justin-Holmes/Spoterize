235e117b67fb350dc0e73f08e6bd1a675e749378
justin <justin.ryan.holmes@icloud.com>
2015-08-11 14:57:03 -0600

    Style modal on index#pandora and re-style playlist view for better UX/UI

--- app/assets/javascripts/loading.js
+++ app/assets/javascripts/loading.js
@@ -1,19 +1,14 @@
 $(document).ready(function(){
   $('#pandora-button').on('click', function (){
     myApp.showPleaseWait();
-    debugger;
   });
 
   var myApp;
   myApp = myApp || (function () {
-    var pleaseWaitDiv = $('<div class="modal fade" id="pleaseWaitDialog" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-body"><h1>Finding Pandora songs...</h1></div></div></div></div>');
+    var pleaseWaitDiv = $('<div class="modal fade" id="pleaseWaitDialog" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header">Finding Pandora songs...</div><div class="modal-body"><i class="fa fa-spinner fa-pulse"></i></div></div></div></div>');
     return {
       showPleaseWait: function() {
         pleaseWaitDiv.modal('show');
-      },
-
-      hidePleaseWait: function () {
-        pleaseWaitDiv.modal('hide');
       }
     };
   })();
