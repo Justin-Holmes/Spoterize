4f83bfbaacf2af34e5df97c504d8a31e32e656c5
justin <justin.ryan.holmes@icloud.com>
2015-08-11 15:16:45 -0600

    Change spotify playlist form width and add loading wheel to modal on pandora#index

--- app/assets/javascripts/loading.js
+++ app/assets/javascripts/loading.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
   var myApp;
   myApp = myApp || (function () {
-    var pleaseWaitDiv = $('<div class="modal fade" id="pleaseWaitDialog" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header">Finding Pandora songs...</div><div class="modal-body"><i class="fa fa-spinner fa-pulse"></i></div></div></div></div>');
+    var pleaseWaitDiv = $('<div class="modal fade" id="pleaseWaitDialog" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header">Fetching Pandora songs... This may take a while</div><div class="modal-body"><i class="fa fa-spinner fa-pulse"></i></div></div></div></div>');
     return {
       showPleaseWait: function() {
         pleaseWaitDiv.modal('show');
